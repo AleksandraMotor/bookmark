ad82634263ec27774860dcd6fc6163f93d55eae3
AleksandraMotor <aleksandramotor@gmail.com>
2023-12-18 02:36:37 +0100

    corrected pop-up

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,5 @@
-import EasySharing from "../features/components/easy-sharing/EasySharing";
+import Button from "../button/Button";
+import ilustration from '../../assets/ilustration/illustration-features-tab-3.svg';
 
 import './Modal.scss';
 
@@ -6,7 +7,16 @@ const Modal: React.FC = () => {
 
     return (
         <div className='modal'>
-            <EasySharing/>
+            <div className='modal__image'>
+                <img src={ilustration} alt="illustration-features-tab-3"/>
+            </div>            
+            <div className='modal__info'>
+                <h2 className='modal__info__title'>Share your bookmarks</h2>
+                <p className='modal__info__text'>
+                    Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.
+                </p>
+                <Button>More Info</Button>
+            </div>
         </div>
     );
 };
